5b76ee1e1255a19148f76531cc659721395a266a
Sreevishnu <sreeab28@gmail.com>
2020-08-05 02:33:11 +0530

    added configuration, Procfile, dummy endpoint

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from logging.config import dictConfig
+import telegram
+from bot_handler.config.credentials import BOT_TOKEN
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    app.logger(BOT_TOKEN)
+    if BOT_TOKEN != None:
+        return {"message": "Bot token loaded successfully"}
+        
