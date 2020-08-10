8fde5f6bce08fc1f8c892d989a265a6e36248e45
Sreevishnu <sreeab28@gmail.com>
2020-08-11 05:03:58 +0530

    testing alternate folder structure

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import request
+import telegram
+from bot_handler.config.credentials import BOT_TOKEN
+from bot_handler.utils.setup_webhook import register_webhook
+from setup import app
+
+bot = telegram.Bot(token=BOT_TOKEN)
 
 
-app_instance = Flask(__name__)
+@app.before_first_request
+def enable_webhook():
+    if not register_webhook(bot):
+        print("Error: Webhook not enabled")
+
+@app.route("/")
+def index():
+    print("Hello World")
+
+    if BOT_TOKEN != None:
+        return {"message": "Bot token loaded successfully"}
+    else:
+        return {"error": "Something went wrong"}
+
+@app.route(f"/{BOT_TOKEN}", methods=["POST"])
+def bot_controller():
+    # print(request.get_json(force=True))
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    print(f"Update: {update}")
+    reply = f"Hey there, {update.message.chat.first_name}" if update.message.text == "/start" else "Noted"
+    bot.sendMessage(
+        chat_id=update.message.chat.id,
+        text=reply,
+        reply_to_message_id=update.message.message_id
+        )
+    return "OK"
 
 if __name__ == "main":
-    app_instance.run(threaded=True, host="0.0.0.0", port=os.environ.get("PORT", 5000))
+    app.run(threaded=True, host="0.0.0.0", port=os.environ.get("PORT", 5000))
