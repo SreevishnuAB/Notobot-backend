9de6f596a1ccc0ee2a6cc8665271dab91e9f9ee6
Sreevishnu <sreeab28@gmail.com>
2020-08-18 03:11:45 +0530

    change table structure

--- bot_handler/model/notes.py
+++ bot_handler/model/notes.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
+
+from sqlalchemy.sql.elements import CollationClause
 from bot_handler.config.db import Base
 
 class Note(Base):
@@ -10,7 +12,8 @@ class Note(Base):
     note_text = Column(String)
     note_type = Column(String, default="text")
     file_id = Column(UUID(as_uuid=True), default=None)
+    file_type = Column(String)
     user_name = Column(String)
 
     def __repr__(self):
-        return f"<Note(id={self.id}, note_text={self.text}, note_type={self.note_type}, file_id={self.file_id}, user_name={self.user})>"
+        return f"<Note(id={self.id}, note_text={self.note_text}, note_type={self.note_type}, file_id={self.file_id}, file_type={self.file_type},user_name={self.user})>"
